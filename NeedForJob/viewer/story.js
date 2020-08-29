dd8af82fa75d39d6121f0a285112c76b9682829d
Mustafa Derman <mustafa.derman@ozu.edu.tr>
2020-08-29 18:39:15 +0300

    telefon ekledik

---
+++ NeedForJob/viewer/story.js
@@ -0,0 +1,260 @@
+var story = {
+"docName": "needforjob",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": false,
+"serverToolsPath":"",
+"fileType":"png",
+"disableHotspots": false,
+"pages": [
+$.extend(new ViewerPage(),{
+"id" : "39DF7100-2BF1-43E2-BEAD-3ABBD92E11C7",
+"index": 0,
+"image": "anasayfa.png",
+"width": 375,
+"height": 812,
+"title": "Anasayfa",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 231,
+			"y": 755,
+			"width": 49,
+			"height": 54
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 82,
+			"y": 755,
+			"width": 58,
+			"height": 54
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 306,
+			"y": 755,
+			"width": 55,
+			"height": 54
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 2
+	},
+	{
+		"rect": {
+			"x": 231,
+			"y": 755,
+			"width": 49,
+			"height": 54
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 3
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "C48DB904-AE17-4126-B85F-C25AE1AF59B3",
+"index": 1,
+"image": "mesajlar.png",
+"width": 375,
+"height": 814,
+"title": "Mesajlar",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 754,
+			"width": 375,
+			"height": 60
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 10,
+			"y": 162,
+			"width": 355,
+			"height": 70
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "F3D4ADC7-415A-48E9-BAE6-EFC771148A1E",
+"index": 2,
+"image": "chat.png",
+"width": 375,
+"height": 814,
+"title": "Chat",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 20,
+			"y": 55,
+			"width": 335,
+			"height": 14
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "9F86CE50-9E30-4997-A698-062619E17630",
+"index": 3,
+"image": "profil.png",
+"width": 375,
+"height": 814,
+"title": "Profil",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 20,
+			"y": 50,
+			"width": 8,
+			"height": 12
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 20,
+			"y": 242,
+			"width": 335,
+			"height": 55
+		},
+		"isParentFixed": false,
+		"page": 4,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "783AE2E6-CC74-4EF3-A513-E39D0807BB3A",
+"index": 4,
+"image": "profil_2.png",
+"width": 375,
+"height": 814,
+"title": "Profil 2",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 9,
+			"y": 44,
+			"width": 126,
+			"height": 28
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "E294B374-3D07-402D-87A4-4E8F8872D529",
+"index": 5,
+"image": "bildirimler.png",
+"width": 375,
+"height": 812,
+"title": "Bildirimler",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 30,
+			"y": 64,
+			"width": 19,
+			"height": 13
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	}
+],
+})
+   ]
+,"resolutions": [1],
+"zoomEnabled": true,
+"title": "NeedForJob",
+"startPageIndex": 0,
+"layersExist": true,
+"centerContent":  false,
+"totalImages": 6,
+"highlightLinks": false
+}
